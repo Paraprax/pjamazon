5f9b04f6f22b4b2983c7ba27b566c05aa2d3ef4a
Paraprax <parkerjohnston28@gmail.com>
2019-01-17 02:32:29 -0500

    wrote seeds file

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -5,10 +5,10 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100),
     price DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     stock_quantity INT(10) NOT NULL DEFAULT 0,
-    PRIMARY KEY (item_id)
+    PRIMARY KEY (id)
 );
---
+++ bamazonSeeds.sql
@@ -0,0 +1,13 @@
+USE bamazon_DB;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Green Paperclip", "Stationary", 0.01, 1),
+("Wakasaki Ninjitsu eBike", "Vehicles", 6999.99, 9),
+("Beanbag Baby Series 1 Complete Collection", "Toys", 1000.00, 7),
+("JAC Makeup Palette", "Cosmetics", 42.00, 70),
+("AudioApe Wireless Earbuds", "Audio", 89.99, 20000),
+("Jude The Obscure - 8th Ed Hardcover", "Books", 13.99, 2),
+("Lightning Brand Weasel Grease", "Tools", 6.99, 200),
+("The Company Men (2010) - DVD", "Entertainment", 9.99, 50),
+("Elephant-Grade Circus Peanuts (In-Shell - 50lb Bag)", "Food and Drink", 18.99, 100),
+("Zylexa Smarthome Speaker", "Appliances", 199.99, 10000)
