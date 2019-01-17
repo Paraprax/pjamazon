f2dfc3e7d73c3b90d2210784ad5e820341606cf6
Paraprax <parkerjohnston28@gmail.com>
2019-01-17 03:26:51 -0500

    all products now display on launch and connection closes after program is run

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,9 +25,31 @@ var connection = mysql.createConnection(
 connection.connect(function(err) { //run the 'connect' method on the 'connection' var to get results
     if(err) throw err; 
     //else if no error
-    console.log("\nWelcome to Bamazon!".bold);
+    console.log("\nWelcome to Pjamazon!".bold);
     console.log("connected as id" + connection.threadId + "\n");
 
-   //present the user with options via this function once the connection is established:
-   //userOptions();
+   //display all the products in the bamazon_DB once the connection is established
+   printDatabase();
 });
+
+function printDatabase() {
+    console.log("Displaying full catalogue available from Pjamazon.... \n");
+    connection.query(
+        "SELECT * FROM products", function(err, res) { //concatenate input args into an SQL command
+        if (err) throw err;
+        console.log(res); // print the whole response from this query to the console
+        userOptions(); //called to present the user with a list of options
+        }
+    )
+};
+
+function userOptions() {
+    console.log("Heeryor options. Haffa nice day.");
+    exitPjamazon();
+}
+
+//close the connection to the SQL database
+function exitPjamazon() {
+    console.log("See you next time.... :)");
+    connection.end();
+}
